7d35fb2f4a9d7e3c47254d6d42bd6f27e5e13b88
Barbara Tules <barbaratules@gmail.com>
2023-10-01 19:45:50 -0300

    inclui o cors na aplicacao

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const express =  require('express')
 const routes  = require('./router/routes')
 const bodyParser = require('body-parser');
+const cors = require('cors')
 const server = express()
 
+server.use(cors())
 server.use(bodyParser.json())
 server.use('/', routes)
 
